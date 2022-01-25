d3f2a675a2828eefaa92e6636e104ebdccad8164
harsh aggarwal <harshaggarwal060@gmail.com>
2022-01-25 18:05:43 +0530

    changed cli command

--- bin/index.js
+++ bin/index.js
@@ -3,14 +3,37 @@
 const yargs = require("yargs");
 const fs = require('fs');
 const path = require("path");
+const { isBuffer } = require("util");
 
-const argv = process.argv[0];
+const argv = process.argv[2];
 
-var dirPath = path.join(process.cwd() ,"testCli");
+if(!argv){
+    console.log("Please provide file path");
+    return;
+}
+
+var projectName;
+switch(argv){
+    case ".":
+        projectName = "";
+        break;
+    default:
+        projectName = argv;
+        break;
+}
+
+console.log(projectName);
+
+
+var dirPath = path.join(process.cwd() ,projectName);
 var ReadFilePath = path.join(__dirname , "../project")
-fs.mkdirSync(dirPath)
 
-const files = ["index.js" , "package.json"]
+if(argv !== ""){
+    fs.mkdirSync(dirPath);
+}
+
+
+const files = ["index.js" , "package.json", "package-lock.json"];
 
 files.map((file)=>{
     var data = fs.readFileSync(`${ReadFilePath}/${file}`);
